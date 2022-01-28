9d2ab1bf93534d4195ce816d4e2e26dbf1eda199
Jennifer Sousa <jennifergaldinodesousa@gmail.com>
2022-01-28 20:19:46 -0300

    2801

---
+++ src/ex070821/Ex01.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { 
+        
+        String nome = "Tiago";
+        int idade = 25;
+        
+        System.out.print("Meu nome é " + nome + " e tenho " + idade + " anos de idade");
+    }
+    
+}
